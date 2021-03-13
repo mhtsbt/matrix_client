d8ad80db4171e7ad404650304576b506cf1853f1
mhtsbt <matthias.hutsebaut@gmail.com>
2021-03-13 20:14:55 +0100

    added deps

--- setup.py
+++ setup.py
@@ -10,6 +10,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/mhtsbt/matrix_client",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'paho-mqtt==1.3.1'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
